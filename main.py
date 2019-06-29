50005392ff6caf1fd9ee3cb9ebfb8a11ed44c473
Vitamin <trysomevitamin@gmail.com>
2019-06-29 10:27:17 +0800

    change main

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ coordinate = [[],[]] # 画图数据
 
 for i in range(100):
 
-    data_now = get_rssi.get_data_online()
-    x, y = classify.classifywknn(data_now,dataset, labels, k=3)
+    data_now = get_rssi.get_data_online() # get data
+    x, y = classify.classifywknn(data_now,dataset, labels, k=3) # location arithmetic
+
     coordinate[0].append(x)
     coordinate[1].append(y)
-    draw.plot_durations(coordinate)
+
+    draw.plot_durations(coordinate) # draw
